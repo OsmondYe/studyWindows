5174dc368e6aeb2e85d01d3c17ddf2e1808e1d53
osmond_ye <osmond.ye@nextlabs.com>
2020-03-24 14:52:31 +0800

    study Right Value reference.

--- Cpp/studyCPP/syntax_move_rvref.cpp
+++ Cpp/studyCPP/syntax_move_rvref.cpp
@@ -3,6 +3,144 @@
 /*
 	move semantic and right-vaule-reference
 */
+
+namespace {
+	const int len = 10000;
+	class BigData {
+	public :
+		int* p;
+		BigData() :p(new int[len]) {}
+
+		BigData(const BigData& rh) :BigData() {
+			for (int i = 0; i < len; i++) {
+				p[i] = rh.p[i];
+			}
+		}
+
+		BigData& operator=(const BigData& rh) {
+			if (this == &rh) {
+				return *this;
+			}
+			delete[]p;
+			p = new int[len];
+			for (int i = 0; i < len; i++) {
+				p[i] = rh.p[i];
+			}
+		}
+		~BigData() {if (p) {delete[] p;}}
+	};
+
+	BigData getData() {
+		BigData rt;
+		rt.p[0] = 1;
+		rt.p[1] = 2;
+		rt.p[2] = 3;
+		return rt;     //copy rt's data to outer,  rt will de-ctor here
+	}
+
+	class BigData2 {
+	public:
+		int* p;
+		BigData2() :p(new int[len]) {}
+
+		BigData2(const BigData2& rh) :BigData2() {
+			for (int i = 0; i < len; i++) {
+				p[i] = rh.p[i];
+			}
+		}
+
+		BigData2& operator=(const BigData2& rh) {
+			if (this == &rh) {
+				return *this;
+			}
+			delete[]p;
+			p = new int[len];
+			for (int i = 0; i < len; i++) {
+				p[i] = rh.p[i];
+			}
+		}
+		~BigData2() {
+			if (p) {
+				delete[] p;
+			}
+		}
+
+		// mov
+		BigData2(BigData2&& rh) :p(0){
+			swap(p, rh.p);  // l <-> r,  and l value will not be used anymore
+		}
+
+		BigData2& operator=(BigData2&& rh) {
+			if (this == &rh) {
+				return *this;
+			}
+			swap(p, rh.p); // l <-> r,  and l value will not be used anymore
+		}
+
+	};
+
+	BigData2 getData2() {
+		BigData2 rt;
+		rt.p[0] = 1;
+		rt.p[1] = 2;
+		rt.p[2] = 3;
+		return rt;     //this version get move-semantic optimize than previous one
+	}
+
+
+	class LRConfused : public BigData2 {
+	public:
+		LRConfused() :BigData2() {}
+
+		LRConfused(const LRConfused& rh) : BigData2(rh) {};
+		LRConfused(LRConfused&& rh) : BigData2(rh) {}; //: BigData2(rh) got problme, will call BigData2(const BigData2& rh)
+
+	};
+}
+
+TEST(Syntax, RVauleReferenceProblemIndroduce) {
+	BigData b;
+	b.p[10] = 5;
+	BigData a =b;  // copy ctor?? how to optimize? 
+	
+	BigData c;
+	c = b;		// equal operator?? how to optimize?
+
+	BigData d = getData(); // problem here in getData();
+	cout << "getData has a temp obj, and copy obj to d, then, obj will call deconstructor\n";
+	cout << "[[optimization direction]] ->  move temp obj to outer, and do not need to call deconsturcotr, outer'will call it\n";
+	cout << " right value ref give me a right to modify some right-value, especially in function's return value\n";
+
+	cout << "Give Bigdata move-consturctor and move assignment\n";
+
+
+	BigData2 e = getData2();  // much faster than getData();
+
+
+
+}
+
+TEST(Syntax, RVauleReferenceProblemIndroduceNoRVO) {
+	for (size_t i = 0; i < len; i++)
+	{
+		auto x = getData();
+	}
+}
+
+TEST(Syntax, RVauleReferenceProblemIndroduceRVO) {
+	for (size_t i = 0; i < len; i++)
+	{
+		auto x = getData2();
+	}
+}
+
+TEST(Syntax, RVauleReferenceProblemIndroduceLRConfused) {
+
+	LRConfused a;
+	LRConfused b = a;             // using copy-ctor   
+	LRConfused c = std::move(a);  // using move_ctor
+}
+
 void arg_value(string str) {	cout << str;}
 void arg_value_ref(string& str) {	cout << str;}
 void arg_value_cref(const string& str) {	cout << str;}
@@ -88,5 +226,21 @@ TEST(Syntax, Move2) {
 	for (auto i : x2) {
 		cout << i;
 	}
+}
+
+/*
+perfect forwarding 
+*/
+namespace {
+	template <typename F>
+	struct reverse_bind {
+		F f_;
+		reverse_bind(F const& f) : f_(f) {}
+
+		template<typename T1, typename T2>
+		void operator()(T1 t1, T2 t2) {
+			f_(t2, t1);
+		}
+	};
 
 }
