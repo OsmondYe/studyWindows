83188327df73040a494fd5a4f40b670f90f60164
osmond_ye <osmond.ye@nextlabs.com>
2020-10-15 18:15:08 +0800

    update

--- Cpp/CppTest/test-windows-api.cpp
+++ Cpp/CppTest/test-windows-api.cpp
@@ -8,6 +8,7 @@
 
 
 
+
 TEST(Windows, VectorAsBuf) {
 	std::vector<wchar_t> buf(1024,0);// buf[1024]={0};
 	int len = ::GetModuleFileNameW(NULL, buf.data(), 1024);
@@ -25,11 +26,7 @@ TEST(Windows, VectorAsBuf) {
 	wcout << buf2;
 }
 
-TEST(Windows, DISABLED_ShellOperation) {
-	const wchar_t* exe = L"D:\\OyeProject\\CSharp\\rmd-windows\\SkydrmLocal\\Debug\\SkydrmLocal.exe";
-	const wchar_t* cmd = L"-share c:\\aaa.txt";
-	::ShellExecuteW(NULL, L"open", exe, cmd, NULL, SW_SHOW);
-}
+
 
 
 TEST(Windows, DynamicDLLLoad) {
@@ -49,90 +46,44 @@ TEST(Windows, DynamicDLLLoad) {
 
 
 
-VOID NTAPI RtlInitUnicodeString(PUNICODE_STRING DestinationString,PCWSTR SourceString);
-
-bool PathIsFolder(const wchar_t* path) {
-	DWORD dwAttrib = GetFileAttributes(path);
-	return ((dwAttrib != INVALID_FILE_ATTRIBUTES) && (dwAttrib & FILE_ATTRIBUTE_DIRECTORY));
-}
-
-
-TEST(Windows, UnicodeString) {
-	UNICODE_STRING   aStr = { 0 };
-	//#pragma comment(lib, "ntdll.lib")
-	RtlInitUnicodeString(&aStr, LR"_(\??\C:\Users\oye\AppData\Local\Microsoft\Office\16.0\Floodgate\Word.CampaignStates.json)_");
-
-	wcout << aStr.Buffer << endl;
-
-	bool isFolder = PathIsFolder(LR"_(\??\c:\aaaaa\)_");
-
-	// change the path, and detoured into another path, begin with D:\Nextlabs_Jail\;
-
-	wstring Jail_Path = LR"_(D:\Nextlabs_Jail\)_";
-
-	wstring org_path = aStr.Buffer;
-
-	// modify algo
-	{
-		if (org_path[5] == L':') {
-			//org_path[5] = L'\\';
-			org_path.erase(5,1);
-		}
-		org_path.insert(4, Jail_Path);
-		Jail_Path = org_path;
-	}
-
-	wcout << Jail_Path << endl;
-
-	wstring folder = org_path.substr(0, org_path.find_last_of(L'\\'));
-
-	// make sure path exist
-	::SHCreateDirectoryEx(NULL, Jail_Path.c_str(), NULL);
-}
-
-
-const wchar_t* replaced_file = LR"_(D:\Nextlabs_Jail\C\Users\oye\Desktop\sfsdfsdf.docx)_";
-const wchar_t* replacement_file = LR"_(D:\Nextlabs_Jail\C\Users\oye\Desktop\~WRD0000.tmp)_";
-const wchar_t* backup_file = LR"_(D:\Nextlabs_Jail\C\Users\oye\Desktop\~WRL0001.tmp)_";
 
 
 
+//TEST(Windows, FCreateFile) {
+//
+////#define GENERIC_READ                     (0x80000000L)
+////#define GENERIC_WRITE                    (0x40000000L)
+////#define GENERIC_EXECUTE                  (0x20000000L)
+////#define GENERIC_ALL                      (0x10000000L)
+//
+////#define CREATE_NEW          1
+////#define CREATE_ALWAYS       2
+////#define OPEN_EXISTING       3
+////#define OPEN_ALWAYS         4
+////#define TRUNCATE_EXISTING   5
+//
+////#define FILE_SHARE_READ                 0x00000001  
+////#define FILE_SHARE_WRITE                0x00000002  
+////#define FILE_SHARE_DELETE               0x00000004 
+//
+//	::CreateFileW(replaced_file, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+//}
 
-TEST(Windows, FCreateFile) {
 
-//#define GENERIC_READ                     (0x80000000L)
-//#define GENERIC_WRITE                    (0x40000000L)
-//#define GENERIC_EXECUTE                  (0x20000000L)
-//#define GENERIC_ALL                      (0x10000000L)
-
-//#define CREATE_NEW          1
-//#define CREATE_ALWAYS       2
-//#define OPEN_EXISTING       3
-//#define OPEN_ALWAYS         4
-//#define TRUNCATE_EXISTING   5
-
-//#define FILE_SHARE_READ                 0x00000001  
-//#define FILE_SHARE_WRITE                0x00000002  
-//#define FILE_SHARE_DELETE               0x00000004 
-
-	::CreateFileW(replaced_file, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
-}
-
-
-TEST(Windows, FReplaceFile) {
-
-	auto rt = ::ReplaceFileW(
-		replaced_file,
-		replacement_file,
-		backup_file,
-		2,
-		0,
-		0
-	);
-
-	EXPECT_EQ(rt , TRUE);
-
-}
+//TEST(Windows, FReplaceFile) {
+//
+//	auto rt = ::ReplaceFileW(
+//		replaced_file,
+//		replacement_file,
+//		backup_file,
+//		2,
+//		0,
+//		0
+//	);
+//
+//	EXPECT_EQ(rt , TRUE);
+//
+//}
 
 
 TEST(Windows, FClearFolder) {
---
+++ Cpp/CppTest/test_win_process.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+
+
+
+TEST(Process, Create) {
+	STARTUPINFO si; //进程启动时需要初始化的结构
+	PROCESS_INFORMATION pi; //进程启动后的有关信息
+	si.cb = sizeof(STARTUPINFO);
+	GetStartupInfo(&si);
+	si.wShowWindow = SW_SHOW;//这里设置窗口为显示，SW_HIDE为隐藏窗口
+	si.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
+	CreateProcess(LR"_(C:\Windows\notepad.exe)_",
+		NULL, NULL, NULL, TRUE, NULL, NULL, NULL, &si, &pi);
+}
+
+TEST(Process, ShellOperation) {
+	//
+	// open webpage
+	//
+	const wchar_t* web = L"www.baidu.com";
+
+	::ShellExecuteW(
+		NULL,		// NULL window
+		L"open",    // verb
+		web,		// lp File 
+		NULL,		// NULL param 
+		NULL,		// NULL dir
+		SW_SHOW     // defualt window show state
+	);
+}
+
+
+TEST(Process, Enum) {
+
+
+}
